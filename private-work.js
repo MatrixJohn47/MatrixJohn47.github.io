430859cbc66658948b5825e6d4b13192e4e8d9c3
Mobin Santhosh John <matrixjohnm0b1n@gmail.com>
2026-08-10 18:26:39 +0530

    Fix image filename format in private-work.js

--- private-work.js
+++ private-work.js
@@ -62,7 +62,7 @@ const socialPosts = [
   { image: '../assets/private-work/social-media/CFO KSA Whatsapp Campaign.png' },
   { image: '../assets/private-work/social-media/2.png' },
   { image: '../assets/private-work/social-media/Brainbox Creative.png' },
-  { image: '../assets/private-work/social-media/DTS Indo 1.1 Whatsapp Campaign 65 days to go.png' },
+  { image: '../assets/private-work/social-media/DTS Indo 1:1 Whatsapp Campaign 65 days to go.png' },
   { image: '../assets/private-work/social-media/SAP_.png' },
   { image: '../assets/private-work/social-media/SGN.png' },
   // { image: '../assets/private-work/social-01.jpg' },
